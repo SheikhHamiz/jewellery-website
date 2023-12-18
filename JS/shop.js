9b71060be978adc750b8102de182952dae07a19e
SheikhHamiz <shkhamiz@gmail.com>
2023-12-18 19:32:29 +0530

    first commit

--- JS/shop.js
+++ JS/shop.js
@@ -1,7 +1,9 @@
 const shopCarousel = document.querySelector(".shop-carousel-slider");
 const slides = Array.from(shopCarousel.children);
 
-console.log(slides);
+const bestSellerSlider = document.querySelector(".best-sellers-slider");
+const leftSlider = document.querySelector(".left-slider");
+const rightSlider = document.querySelector(".right-slider");
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
@@ -28,3 +30,12 @@ this.setInterval( () => {
     currentSlide.classList.remove("current-slide");
     nextSlide.classList.add("current-slide");
 },3000);
+let j = 0;
+leftSlider.addEventListener("click", ()=> {
+    if(j == 0) bestSellerSlider.style.transform = "translateX(-100%)";
+    j = 1;
+});
+rightSlider.addEventListener("click", ()=> {
+    bestSellerSlider.style.transform = "translateX(0%)";
+    j = 0;
+});
